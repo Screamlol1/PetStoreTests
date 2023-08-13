04f43d013f8c217b5709e68cb9d6d364fce22c84
cyril <cyril2173@gmailcom>
2023-08-13 18:56:16 +0300

    Add logger

--- PetStoreTests/lib/my_requests.py
+++ PetStoreTests/lib/my_requests.py
@@ -1,5 +1,5 @@
 import requests
-
+from lib.logger import Logger
 
 class MyRequests():
     @staticmethod
@@ -27,6 +27,8 @@ class MyRequests():
         if cookies is None:
             cookies = {}
 
+        Logger.add_request(url, data, headers, cookies, method)
+
         if method == 'GET':
             response = requests.get(url, json=data, headers=headers, cookies=cookies)
         elif method == 'POST':
@@ -37,5 +39,7 @@ class MyRequests():
             response = requests.delete(url, json=data, headers=headers, cookies=cookies)
         else:
             raise Exception(f"Bad http mthod '{method}' was received")
+
+        Logger.add_response(response)
 
         return response
